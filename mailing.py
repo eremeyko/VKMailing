b9676bd49bc5015a2ce3eac2212ab9c9e7198572
Не Еремей! <60701195+eremeyko@users.noreply.github.com>
2022-06-26 14:50:54 +0300

    Исправлена и улучшена работа
    Исправлено:
    - Раньше было невозможно получить всех друзей, сейчас возвращается список со всеми друзьями на аккаунте
    - Логика нумерации аккаунтов
    - Обращение к функции смене аккаунта (могла произойти ошибка)
    
    Добавлено:
    - Варианты действий при возникновении дополнительных ошибок
    - Рекомендация по таймингу
    
    Хотелки:
    - Сменить запрос с messages.send на execute

--- mailing.py
+++ mailing.py
@@ -7,26 +7,30 @@ import vk_api
 import vk_captchasolver as vc
 
 i = 0
+timing = 0
+
+accounts = open("accounts.txt", "r").read().splitlines()
+message = open("messages.txt", "r").read().split("|")
 
 
-def authvk(strt, accounts):
+def authvk(accounts):
     global vk
     global i
-    while True:
+    for n, j in enumerate(accounts, start=1):
         try:
-            print(f"[#{strt+1} accounts.txt] Попытка входа в аккаунт...")
+            print(f"[#{n + i} accounts.txt] Попытка входа в аккаунт...")
             vk_session = vk_api.VkApi(
-                login=accounts[strt].split(":")[0],
-                password=accounts[strt].split(":")[1],
+                login=j.split(":")[0],
+                password=j.split(":")[1],
                 app_id=2685278,
                 scope=4096,
             )
             vk_session.auth()
             vk = vk_session.get_api()
 
-            if vk is not None:
-                print(f"[#{strt+1} accounts.txt] Успешно вошли.")
-                return vk
+            print(f"[#{n + i} accounts.txt] Успешно вошли.")
+            i += n
+            return vk
 
         except IndexError:
             print("  [!accounts.txt] Уберите лишние пробелы или добавьте аккаунты.")
@@ -35,8 +39,11 @@ def authvk(strt, accounts):
 
         except vk_api.exceptions.AuthError:
             print("  [!accounts.txt] Токен невалид!")
-            i += 1
-            strt += 1
+
+        except Exception as e:
+            print(f"  [!!!] {e}")
+            system("pause")
+            exit()
 
 
 def checkFiles(accounts, mess):
@@ -62,67 +69,85 @@ def getUsers(vk):
 
     for i in range(2):
         friendsList = vk.friends.get(
-            fields="can_write_private_message", count=5000, offset=i
+            fields="can_write_private_message", count=5000, offset=i * 5000
         )["items"]
+
         for friend in friendsList:
-            if friend["can_write_private_message"] == 1 and not friend.get(
-                "deactivated"
-            ):
+            if friend["can_write_private_message"] == 1 and not friend.get("deactivated"):
                 friendsIds.append(friend["id"])
+
     return shuffle(friendsIds)
 
 
 def main(i, mess, accounts, timing):
-    vk = authvk(i, accounts)
-    userlist = getUsers(vk)
-    for id in userlist:
-        sleep(timing)
-        try:
-            vk.messages.send(user_id=id, random_id=0, message=choice(mess))
-            print(f"[#{i+1}] Сообщение id{id} отправлено.")
+    if vk := authvk(accounts):
+        userlist = getUsers(vk)
+        for id in userlist:
+            try:
+                sleep(timing)
+                message = choice(mess)
+                vk.messages.send(user_id=id, random_id=0, message=message)
+                print(f"[#{i+1}] Сообщение id{id} отправлено.")
 
-        except vk_api.exceptions.Captcha as captcha:
-            print("  [?]Ой, капча...")
-            captcha_handler(captcha)
-            print(f"[#{i+1}] Сообщение id{id} отправлено.")
+            except vk_api.exceptions.Captcha as captcha:
+                print("  [?]Ой, капча...")
+                captcha_handler(captcha)
+                print(f"[#{i+1}] Сообщение id{id} отправлено.")
 
-        except vk_api.exceptions.ApiError as e:
-            if e.code == 6:
-                print(f"  [!{i+1}] Слишком быстро\n  [•{i+1}] Ждем 2 секунды...")
-                sleep(2)
+            except vk_api.exceptions.ApiError as e:
+                if e.code == 6:
+                    print(f"  [!{i}] Слишком быстро\n  [•{i+1}] Ждем 2 секунды...")
+                    sleep(2)
 
-            if e.code == 7:
-                print(f"  [!{i+1}] Лимит\n  [•{i+1}] Меняем аккаунт...")
-                i += 1
-                vk = authvk(i, accounts)
-                userlist = getUsers(vk)
+                elif e.code == 7:
+                    print(f"  [!{i}] Лимит\n  [•{i}] Меняем аккаунт...")
+                    vk = authvk(accounts)
+                    userlist = getUsers(vk)
 
-            elif e.code == 5:
-                print(f"  [!{i+1}] Невалид\n  [•{i+1}] Меняем аккаунт...")
-                i += 1
-                vk = authvk(i, accounts)
-                userlist = getUsers(vk)
+                elif e.code == 5:
+                    print(f"  [!{i}] Невалид\n  [•{i + 1}] Меняем аккаунт...")
+                    vk = authvk(accounts)
+                    userlist = getUsers(vk)
 
-            else:
-                print(f"{e.code}:{e}\n[?] Обратитесь к создателю")
-                system("pause")
-                exit()
+                elif e.code == 17:
+                    print(f"  [!{i}] Неожиданная валидация\n  [•{i + 1}] Меняем аккаунт...")
+                    vk = authvk(accounts)
+                    userlist = getUsers(vk)
+
+                elif e.code == 29:
+                    print(f"  [!{i}] Лимит на методе\n  [•{i + 1}] Меняем аккаунт...")
+                    vk = authvk(accounts)
+                    userlist = getUsers(vk)
+
+                elif e.code == 900:
+                    print(f"  [!{i}] {id} в черном списке в заданном аккаунте")
+                    pass
+
+                elif e.code == 902:
+                    print(f"  [!{i}] {id} нельзя отправить сообщение")
+                    pass
+
+                elif e.code == 914:
+                    print(f"  [!{i}] Очень длинный текст сообщения, ВК не пропускает!\n  Измените, текст в message.txt:\n{message}")
+                    system("pause")
+                    exit()
+
+                else:
+                    print(f"  {e.code}:{e}\n  [?] Обратитесь к создателю\tпж")
+                    system("pause")
+                    exit()
 
 
-if __name__ == "__main__":
-    timing = 0
-    accounts = open("accounts.txt", "r").read().splitlines()
-    mess = open("messages.txt", "r").read().split("|")
+checkFiles(accounts, message)
 
-    checkFiles(accounts, mess)
+while timing <= 0:
+    try:
+        timing = int(input("[!] Введите задержку между отправкой сообщений\n  \
+Рекомендуется 5-10 секунд для лучшей работы\n  "))
+    except ValueError:
+        system("cls")
 
-    system("cls")
-    while timing <= 0:
-        try:
-            timing = int(input("[!] Введите задержку между отправкой сообщений\n  "))
-        except ValueError:
-            pass
+main(i, message, accounts, timing)
 
-    main(i, mess, accounts, timing)
-    print("[:)] Рассылка окончена")
-    system("pause")
+print("[:)] Рассылка окончена")
+system("pause")
